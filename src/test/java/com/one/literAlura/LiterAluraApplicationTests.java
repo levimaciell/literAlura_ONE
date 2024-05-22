81123abfe71ef06c0a43cb4300d3189269e97eba
levimaciell <levimaciell1221@gmail.com>
2024-05-22 08:45:39 -0300

    Initial commit

---
+++ src/test/java/com/one/literAlura/LiterAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one.literAlura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
